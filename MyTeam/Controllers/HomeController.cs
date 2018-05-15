36dd7f3e5a4987fe8a04ac8526c4105956427e8d
zm <zm@ZM-PC>
2018-05-15 17:20:19 +0800

    调整邮件通知内容

--- MyTeam/Controllers/HomeController.cs
+++ MyTeam/Controllers/HomeController.cs
@@ -9,13 +9,16 @@ namespace MyTeam.Controllers
 {
     public class HomeController : BaseController
     {
+        const int OUTPOOL_DELAY_MONTHS = -3; // 超过3个月未出池
+        const int INPOOL_DELAY_DAYS = -4; // 超过4天未入池
+
         //
         // GET: /Home/
         public ActionResult Index()
         {
             User user = this.GetSessionCurrentUser();
 
-            HomeResult hr = GetHomeResult(user);                         
+            HomeResult hr = GetHomeResult(user);
 
             return View(hr);
         }
@@ -49,7 +52,23 @@ namespace MyTeam.Controllers
         public ActionResult Notify(string id)
         {
             User u = GetUserList().Find(p => p.Username == id);
-            return View(GetHomeResult(u, true));
+            if(u == null)
+            {
+                return null;
+            }
+            var uid = u.UID;
+
+            // 未入池的详细信息
+            var delayDate = DateTime.Now.AddDays(INPOOL_DELAY_DAYS);
+            var inPoolDelay = dbContext.ReqMains.Where(p => p.ProcessStat == (int)ReqProcessStatEnums.研发评估 && p.AcptDate < delayDate && p.ReqAcptPerson == uid).ToList();
+            
+            // 延期的项目
+            var projs = dbContext.Projs.Where(p => p.ProjStat == (int)ProjStatEnums.进行中 && p.ReqAnalysisID == uid).ToList();
+            var projDelay = getProjDelays(projs);
+
+            NotifyInfo notifyInfo = new NotifyInfo { InPoolDelay = inPoolDelay, ProjDelay = projDelay };
+
+            return View(notifyInfo);
         }
 
         /// <summary>
@@ -65,7 +84,7 @@ namespace MyTeam.Controllers
             {
                 foreach (var u in GetFormalUserList())
                 {
-                    userList.Add(u.Username);                    
+                    userList.Add(u.Username);
                 }
 
                 return string.Join(",", userList.ToArray());
@@ -77,12 +96,12 @@ namespace MyTeam.Controllers
         }
 
         /// <summary>
-        /// 公共方法：获取提醒结果
+        /// 获取提醒结果
         /// </summary>
         /// <param name="user"></param>
         /// <param name="isNotify"></param>
         /// <returns></returns>
-        protected HomeResult GetHomeResult(User user = null, bool isNotify = false)
+        protected HomeResult GetHomeResult(User user = null)
         {
             var isAdmin = true;
 
@@ -105,70 +124,63 @@ namespace MyTeam.Controllers
 
             hr.UID = uid;
 
-            string sql = "SELECT main.SysID, count(1) as ReqNum, {0} as ReqAcptPerson FROM ReqMains main LEFT JOIN ReqDetails detail ON main.ReqMainID = detail.ReqMainID WHERE detail.ReqStat = {1} {3}" ; //组织SQL语句
+            string sql = "SELECT main.SysID, count(1) as ReqNum, {0} as ReqAcptPerson FROM ReqMains main LEFT JOIN ReqDetails detail ON main.ReqMainID = detail.ReqMainID WHERE detail.ReqStat = {1} {3}"; //组织SQL语句
 
             if (!isAdmin)
             {
                 // 非Admin则SQL增加用户信息
-                sql += " and main.SysID in (select rs.SysID from RetailSystems rs where rs.ReqPersonID = {0}) ";
+                //sql += " and main.SysID in (select rs.SysID from RetailSystems rs where rs.ReqPersonID = {0}) ";
+                sql += " and main.ReqAcptPerson = {0}"; // 谁受理，提醒谁
             }
 
             sql += " {2} GROUP BY main.SysID";
 
-            if (isNotify)
+
+            hr.ReqLS = dbContext.Database.SqlQuery<HomeReq>(string.Format(sql, uid, (int)ReqStatEnums.入池, "", "")).ToList();
+            hr.ReqInpoolLS = dbContext.Database.SqlQuery<HomeReq>(string.Format(sql, uid, (int)ReqStatEnums.待评估, "", "")).ToList();
+
+
+            hr.ReqDelayLS = dbContext.Database.SqlQuery<HomeReq>(string.Format(sql, uid, (int)ReqStatEnums.入池, "and main.AcptDate <= DATEADD(month,"+OUTPOOL_DELAY_MONTHS+",GETDATE())", "")).ToList();
+            hr.ReqInpoolDelayLS = dbContext.Database.SqlQuery<HomeReq>(string.Format(sql, uid, (int)ReqStatEnums.待评估, "and main.AcptDate <= DATEADD(day,"+INPOOL_DELAY_DAYS+",GETDATE())", "and main.ProcessStat = " + (int)ReqProcessStatEnums.研发评估)).ToList(); // 对于拟稿人办理的不跟踪
+
+
+            // 统计计算未出池的需求总数
+            int reqLsSum = 0;
+            foreach (HomeReq q in hr.ReqLS)
             {
-                hr.ReqLS = null;
-                hr.ReqInpoolLS = null;
+                reqLsSum += q.ReqNum;
             }
-            else
+            hr.ReqLsSum = reqLsSum;
+
+            // 统计计算未入池的需求总数
+            int reqInpoolLsSum = 0;
+            foreach (HomeReq q in hr.ReqInpoolLS)
             {
-                hr.ReqLS = dbContext.Database.SqlQuery<HomeReq>(string.Format(sql, uid, (int)ReqStatEnums.入池, "", "")).ToList();
-                hr.ReqInpoolLS = dbContext.Database.SqlQuery<HomeReq>(string.Format(sql, uid, (int)ReqStatEnums.待评估, "", "")).ToList();
+                reqInpoolLsSum += q.ReqNum;
             }
+            hr.ReqInpoolLsSum = reqInpoolLsSum;
 
-            hr.ReqDelayLS = dbContext.Database.SqlQuery<HomeReq>(string.Format(sql, uid, (int)ReqStatEnums.入池, "and main.AcptDate <= DATEADD(month,-3,GETDATE())", "")).ToList();
-            hr.ReqInpoolDelayLS = dbContext.Database.SqlQuery<HomeReq>(string.Format(sql, uid, (int)ReqStatEnums.待评估, "and main.AcptDate <= DATEADD(day,-4,GETDATE())", "and main.ProcessStat = "+(int)ReqProcessStatEnums.研发评估)).ToList(); // 对于拟稿人办理的不跟踪
-
-            if (!isNotify) //notify时不需要计算
+            // 统计计算超期未出池的需求总数
+            int reqDelayLsSum = 0;
+            foreach (HomeReq q in hr.ReqDelayLS)
             {
-                // 统计计算3个月未出池的需求总数
-                int reqLsSum = 0;
-                foreach (HomeReq q in hr.ReqLS)
-                {
-                    reqLsSum += q.ReqNum;
-                }
-                hr.ReqLsSum = reqLsSum;
-
-                // 统计计算4天未入池的需求总数
-                int reqInpoolLsSum = 0;
-                foreach (HomeReq q in hr.ReqInpoolLS)
-                {
-                    reqInpoolLsSum += q.ReqNum;
-                }
-                hr.ReqInpoolLsSum = reqInpoolLsSum;
-
-                // 统计计算三个月未出池的需求总数
-                int reqDelayLsSum = 0;
-                foreach (HomeReq q in hr.ReqDelayLS)
-                {
-                    reqDelayLsSum += q.ReqNum;
-                }
-
-                hr.ReqDelayLsSum = reqDelayLsSum;
-
-                // 统计计算超过4天未入池的需求总数
-                int reqInpoolDelayLsSum = 0;
-                foreach (HomeReq q in hr.ReqInpoolDelayLS)
-                {
-                    reqInpoolDelayLsSum += q.ReqNum;
-                }
-
-                hr.ReqInpoolDelayLsSum = reqInpoolDelayLsSum;
-
+                reqDelayLsSum += q.ReqNum;
             }
+
+            hr.ReqDelayLsSum = reqDelayLsSum;
+
+            // 统计计算超期未入池的需求总数
+            int reqInpoolDelayLsSum = 0;
+            foreach (HomeReq q in hr.ReqInpoolDelayLS)
+            {
+                reqInpoolDelayLsSum += q.ReqNum;
+            }
+
+            hr.ReqInpoolDelayLsSum = reqInpoolDelayLsSum;
+
+
 
             //////////////////////////////////////////////////////////////////////
-
             // 筛选出各个阶段延期的项目（只统计项目状态为：进行中）
             //List<ProjPlan> plans = dbContext.ProjPlans.ToList();
 
@@ -186,11 +198,93 @@ namespace MyTeam.Controllers
             // 顺便统计进行中的项目有多少
             ViewBag.ProjsInProcessNum = projs.Count;
 
+            // 接下来判断延期的具体，信息，提取到公共方法
+
+            List<HomeProjDelay> delays = getProjDelays(projs);
+
+            hr.ProjDetails = delays;
+
+            //////////////////////////////////////////////////////////////////////
+
+            // 列出超过计划下发日期仍未下发的
+            // 2018年2月9日修改：同时统计所有正在进行中的下发
+            var rlsLs = dbContext.ReqReleases.Where(p => p.ReleaseDate == null);
+            if (!isAdmin)
+            {
+                rlsLs = rlsLs.Where(p => p.DraftPersonID == uid);
+            }
+
+            ViewBag.ReleasesInProcessNum = rlsLs.Count();
+
+            var d = DateTime.Now.AddDays(-1);
+
+            hr.RlsDelayLS = rlsLs.Where(p => p.PlanReleaseDate < d).ToList();
+
+            return hr;
+        }        
+
+        /// <summary>
+        /// 右上角用户信息和登录退出分部视图
+        /// </summary>
+        /// <returns></returns>
+        public PartialViewResult LoginPartial()
+        {
+            if (Session == null || Session["Realname"] == null)
+            {
+                var user = this.GetSessionCurrentUser();
+                Session["Realname"] = user.Realname;
+                Session["IsAdmin"] = user.IsAdmin;
+            }
+
+            return PartialView();
+        }
+
+        /* 
+         * 2017年2月27日：去除主题相关功能
+         * 
+        /// <summary>
+        /// 获取主题
+        /// </summary>
+        /// <returns></returns>
+        public string GetTheme()
+        {
+            // 通过cookie判断主题
+            var theme = "navbar-inverse";
+            if (Request.Cookies["theme"] != null)
+            {
+                theme = "navbar-" + Request.Cookies["theme"].Value;
+            }
+
+            return theme;
+        }
+
+        /// <summary>
+        /// 记录主题
+        /// </summary>
+        /// <param name="theme"></param>
+        /// <returns></returns>
+        public string SetTheme(string theme)
+        {
+            Response.Cookies["theme"].Value = theme;
+            Response.Cookies["theme"].Expires = DateTime.MaxValue;
+
+            return Constants.AJAX_RESULT_SUCCESS;
+        }
+        */
+
+
+        /// <summary>
+        /// 获取项目延期的具体情况
+        /// </summary>
+        /// <param name="projs"></param>
+        /// <returns></returns>
+        private List<HomeProjDelay> getProjDelays(List<Proj> projs)
+        {
             List<HomeProjDelay> delays = new List<HomeProjDelay>();
 
             foreach (Proj p in projs) //从筛选出的项目中，寻找项目计划
             {
-                ProjPlan plan = dbContext.ProjPlans.Where(a => a.ProjID == p.ProjID).FirstOrDefault();                
+                ProjPlan plan = dbContext.ProjPlans.Where(a => a.ProjID == p.ProjID).FirstOrDefault();
 
                 // 如果筛选出项目在项目计划列表中，那么判断时间是否延期
                 if (plan != null)
@@ -244,78 +338,12 @@ namespace MyTeam.Controllers
                         delays.Add(projDelay);
                         continue;
                     }
-                }                
-            }
-            hr.ProjDetails = delays;
-
-            //////////////////////////////////////////////////////////////////////
-
-            // 列出超过计划下发日期仍未下发的
-            // 2018年2月9日修改：同时统计所有正在进行中的下发
-            var rlsLs = dbContext.ReqReleases.Where(p => p.ReleaseDate == null);
-            if(!isAdmin)
-            {
-                rlsLs = rlsLs.Where(p => p.DraftPersonID == uid);
+                }
             }
 
-            ViewBag.ReleasesInProcessNum = rlsLs.Count();
-
-            var d = DateTime.Now.AddDays(-1);
-
-            hr.RlsDelayLS = rlsLs.Where(p => p.PlanReleaseDate < d).ToList();        
-
-            return hr;
+            return delays;
         }
 
-        /// <summary>
-        /// 右上角用户信息和登录退出分部视图
-        /// </summary>
-        /// <returns></returns>
-        public PartialViewResult LoginPartial()
-        {
-            if (Session == null || Session["Realname"] == null)
-            {
-                var user = this.GetSessionCurrentUser();
-                Session["Realname"] = user.Realname;
-                Session["IsAdmin"] = user.IsAdmin;
-            }
-
-            return PartialView();
-        }
-
-        /* 
-         * 2017年2月27日：去除主题相关功能
-         * 
-        /// <summary>
-        /// 获取主题
-        /// </summary>
-        /// <returns></returns>
-        public string GetTheme()
-        {
-            // 通过cookie判断主题
-            var theme = "navbar-inverse";
-            if (Request.Cookies["theme"] != null)
-            {
-                theme = "navbar-" + Request.Cookies["theme"].Value;
-            }
-
-            return theme;
-        }
-
-        /// <summary>
-        /// 记录主题
-        /// </summary>
-        /// <param name="theme"></param>
-        /// <returns></returns>
-        public string SetTheme(string theme)
-        {
-            Response.Cookies["theme"].Value = theme;
-            Response.Cookies["theme"].Expires = DateTime.MaxValue;
-
-            return Constants.AJAX_RESULT_SUCCESS;
-        }
-        */
- 
     }
-       
+
 }
---
+++ MyTeam/Models/HomeView/NotifyInfo.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+
+namespace MyTeam.Models
+{
+    public class NotifyInfo
+    {
+        public List<ReqMain> InPoolDelay { get; set; }
+
+        public List<HomeProjDelay> ProjDelay { get; set; }
+    }
+}
